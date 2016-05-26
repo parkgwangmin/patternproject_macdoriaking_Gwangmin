796793256a9f03616f40523bfaec8edf5ad168b4
박광민 <qra0428@naver.com>
2016-05-26 12:30:02 +0900

    textArea로 수정.

--- src/macorder/OrderPanel.java
+++ src/macorder/OrderPanel.java
@@ -32,11 +32,12 @@ public class OrderPanel extends JPanel implements ActionListener {
 
     private JLabel Bemenu;
     private JButton bt_Americano, bt_Ice, take_OK_cafe;
-    private JButton bt_Sanghai, bt_Bulgogi, bt_SetMenu,bt_Coke,bt_French, take_OK_burger;
+    private JButton bt_Sanghai, bt_Bulgogi, bt_SetMenu, bt_Coke, bt_French, take_OK_burger;
     private OrderControl orderCTL;
     private Macafe macafe;
     private Burger burger;
-    private JTextField textfd;
+    private JTextArea textfd;
+    public int cafestock = 10;
 
     public OrderPanel() {
         setLayout(null);
@@ -51,12 +52,10 @@ public class OrderPanel extends JPanel implements ActionListener {
     public OrderPanel(Burger burger) {
         this.burger = burger;
     }
-    
-    
 
     public void makeBurgerView() {
-        
-        textfd = new JTextField(100);
+
+        textfd = new JTextArea();
         textfd.setBounds(550, 20, 400, 150);
         add(textfd);
 
@@ -83,27 +82,27 @@ public class OrderPanel extends JPanel implements ActionListener {
         bt_Ice.setBounds(100, 670, 100, 50);
         bt_Ice.addActionListener(this);
         add(bt_Ice);
-        
+
         take_OK_cafe = new JButton("음료주문");
         take_OK_cafe.setBounds(260, 670, 100, 50);
         take_OK_cafe.addActionListener(this);
         add(take_OK_cafe);
-        
+
         bt_Coke = new JButton("콜라");
         bt_Coke.setBounds(100, 240, 100, 50);
         bt_Coke.addActionListener(this);
         add(bt_Coke);
-        
+
         bt_French = new JButton("후렌치후라이");
         bt_French.setBounds(230, 240, 130, 50);
         bt_French.addActionListener(this);
         add(bt_French);
-        
+
         bt_SetMenu = new JButton("세트메뉴");
         bt_SetMenu.setBounds(200, 340, 150, 50);
         bt_SetMenu.addActionListener(this);
         add(bt_SetMenu);
-        
+
         take_OK_burger = new JButton("버거주문");
         take_OK_burger.setBounds(200, 410, 150, 50);
         take_OK_burger.addActionListener(this);
@@ -111,36 +110,39 @@ public class OrderPanel extends JPanel implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        
+
         if (e.getSource() == bt_Americano) {
             System.out.println("아메리카노 선택");
+            textfd.setText("아메리카노 선택\n");
             Macafe americano = new Americano();
             AmericanoCommand makeAmericano = new AmericanoCommand((Americano) americano);
             orderCTL.setCommand(makeAmericano);
             macafe = americano;
         }
-        
+
         /**
          * 데코레이터 버튼
          */
         if (e.getSource() == bt_Ice) {
             System.out.println("얼음추가 선택");
+            textfd.append("얼음추가 선택\n");
             macafe = new Ice(macafe);
         }
-        
+
         //주문하기
-        if(e.getSource() == take_OK_cafe){
+        if (e.getSource() == take_OK_cafe) {
             orderCTL.startOrder();
-            
+            cafestock--;
+
+//            String cost = String.valueOf(macafe.cost()); 혹시나 String으로 출력해야한다면..
             System.out.println("주문결과 : " + macafe.getDescription());
             System.out.println("가격 : " + macafe.cost());
             macafe.cost();
-            textfd.setText("주문결과 : "+ macafe.getDescription()+"\n"
-                            +"가격 : "+ macafe.cost());
-            
-            
+            textfd.append("주문결과 : " + macafe.getDescription() + "\n"
+                    + "가격 : " + macafe.cost() + "\n"
+                    + "재고량 : "+ cafestock);
         }
-        
+
         /**
          * 햄버거 종류
          */
@@ -160,29 +162,28 @@ public class OrderPanel extends JPanel implements ActionListener {
             burger = bulgogi;
             bt_Sanghai.setEnabled(false);
         }
-        
+
         /**
          * 데코레이터 버튼
          */
-        
-        if(e.getSource() == bt_Coke){
+        if (e.getSource() == bt_Coke) {
             System.out.println("콜라 추가선택 + 1500원");
             burger = new Coke(burger);
         }
-        
-        if(e.getSource() == bt_French){
+
+        if (e.getSource() == bt_French) {
             System.out.println("후렌치후라이 추가선택 + 1500원");
             burger = new FrenchFries(burger);
         }
-        
-        if(e.getSource() == bt_SetMenu){
+
+        if (e.getSource() == bt_SetMenu) {
             System.out.println("세트메뉴선택 + 2000원");
             burger = new SetBurgerMenu(burger);
-        }        
-        
-        if(e.getSource() == take_OK_burger){
+        }
+
+        if (e.getSource() == take_OK_burger) {
             orderCTL.startOrder();
-            
+
             System.out.println("주문결과 : " + burger.getDescription());
             System.out.println("가격 : " + burger.cost());
             burger.cost();
